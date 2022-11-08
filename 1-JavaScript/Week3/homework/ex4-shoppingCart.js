5e16c0a6fc405802eeba355d85f68e56d673853e
eminalpernl <eminalpernl@gmail.com>
2022-11-08 14:25:56 +0100

    ex-4 is done

--- 1-JavaScript/Week3/homework/ex4-shoppingCart.js
+++ 1-JavaScript/Week3/homework/ex4-shoppingCart.js
@@ -23,8 +23,12 @@ you have more than 3 items in your shopping cart the first item gets taken out.
 const shoppingCart = ['bananas', 'milk'];
 
 // ! Function to be tested
-function addToShoppingCart(/* parameters go here */) {
-  // TODO complete this function
+function addToShoppingCart(newGroceryItem) {
+  shoppingCart.push(newGroceryItem);
+  if (shoppingCart.length > 3) {
+    const boughtItem = shoppingCart.shift();
+    return 'you bought' + boughtItem.join(', ') + '.';
+  }
 }
 
 // ! Test functions (plain vanilla JavaScript)
