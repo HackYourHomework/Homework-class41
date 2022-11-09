8b140df98cfbf914549c2e880754dc34bd6021ee
Sergey Zolotko <sergiozoloto@gmail.com>
2022-11-09 12:27:05 +0100

    Completed ex3-w4-JS

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,27 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(arr, itemToBeDeleted) {
+  return arr.filter((basketItem) => basketItem !== itemToBeDeleted);
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const expected = 2;
+    const returned = sanitizeFruitBasket.length;
+    expect(returned).toEqual(expected);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
-    // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(fruitBasket).toEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    const returned = sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(returned).toEqual(expected);
   });
 });
