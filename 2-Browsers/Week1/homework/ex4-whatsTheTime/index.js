2a58ea6c94434cf07716785b053ff2aeac83b11a
Vladimir I <intrustonly@gmail.com>
2022-11-22 21:09:55 +0100

    I finished all exercises and made tests for Browsers week 1.

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -8,7 +8,17 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
+const time = new Date();
+time.toLocaleTimeString();
+const old = document.getElementById("time");
+  if (old !== null) 
+      {old.remove()}
+const timeOnSite = document.createElement('p');
+timeOnSite.id = 'time';
+timeOnSite.textContent = `Current DateTime: ${time}`;
+document.body.appendChild(timeOnSite);
+return timeOnSite, console.log (time);
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+setInterval(addCurrentTime, 1000);
+window.addEventListener('load', addCurrentTime);
