7165b24d25060e2a07307818f7230e258cacfd92
eminalpernl <eminalpernl@gmail.com>
2022-11-24 12:12:13 +0100

    some fixes were applied based on the mentor's feedback forthe EX-2 browsers

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*    Earlier codes
 const nickName = document.getElementById('nickname');
 const favFood = document.getElementById('fav-food');
 const homeTown = document.getElementById('hometown');
@@ -9,6 +9,25 @@ nickName.appendChild(document.createTextNode('Bumin'));
 favFood.appendChild(document.createTextNode('Brocoli soup'));
 
 homeTown.appendChild(document.createTextNode('Istanbul'));
+
+const listItems = document.querySelectorAll('li');
+
+for (let index = 0; index < listItems.length; index++) {
+  listItems[index].className = 'list-item';
+}
+*/
+
+// new code was asked by YaseR Shomaf request some changes;
+
+const nickName = document.getElementById('nickname');
+const favFood = document.getElementById('fav-food');
+const homeTown = document.getElementById('hometown');
+
+nickName.textContent = 'Bumin Kaan';
+
+favFood.textContent = 'Brocoli soup';
+
+homeTown.textContent = 'Istanbul';
 
 const listItems = document.querySelectorAll('li');
 
