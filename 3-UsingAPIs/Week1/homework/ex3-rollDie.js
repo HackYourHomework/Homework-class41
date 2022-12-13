bc206f0f1dd58b08b901b777714c87154f6cd3eb
Hussein <husseinfaqih@gmail.com>
2022-12-13 14:29:26 +0100

    ex3

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -11,48 +11,39 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-// TODO Remove callback and return a promise
-function rollDie(callback) {
-  // Compute a random number of rolls (3-10) that the die MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
+function rollDie() {
+  return new Promise((resolve, reject) => {
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+    const rollOnce = (roll) => {
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Die rolled off the table.'));
-    }
+      if (roll > 6) {
+        reject(Error('Oops... Die rolled off the table.'));
+      }
+      if (roll === randomRollsToDo) {
+        resolve(`Success! Die settled on ${value}.`);
+      }
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
 
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
-
-  // Start the initial roll
-  rollOnce(1);
+    rollOnce(1);
+  });
 }
 
 function main() {
-  // TODO Refactor to use promise
-  rollDie((error, value) => {
-    if (error !== null) {
-      console.log(error.message);
-    } else {
-      console.log(`Success! Die settled on ${value}.`);
-    }
-  });
+  rollDie()
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 // ! Do not change or remove the code below
@@ -60,3 +51,31 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDie;
+
+/*
+>>>>> First i am having this error:
+
+Die scheduled for 10 rolls...
+Die value is now: 2
+Die value is now: 5
+Die value is now: 4
+Die value is now: 4
+Die value is now: 3
+Die value is now: 3
+Die value is now: 5
+Error: Oops... Die rolled off the table.
+    at rollOnce (c:\Users\husse\HYF\MODULE_2\Homework-class41\3-UsingAPIs\Week1\homework\ex3-rollDie.js:24:16)
+    at Timeout._onTimeout (c:\Users\husse\HYF\MODULE_2\Homework-class41\3-UsingAPIs\Week1\homework\ex3-rollDie.js:31:26)
+    at listOnTimeout (node:internal/timers:559:17)
+    at processTimers (node:internal/timers:502:7)
+Die value is now: 4
+Die value is now: 4
+Die value is now: 2
+
+>>>>> The problem is still occurring and this is because whenever the randomRollsToDo is > 6,
+both following conditions will apply at a certain point:
+      if (roll > 6) {}
+      and
+      if (roll < randomRollsToDo)
+
+*/
